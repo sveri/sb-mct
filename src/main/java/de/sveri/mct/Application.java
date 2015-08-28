c4e4ccf8ed311589f9d373ff3257b8bc97f84ff7
sveri <sveri@sveri.de>
2015-08-28 23:13:29 +0200

    assets and base template

--- src/main/java/de/sveri/mct/Application.java
+++ src/main/java/de/sveri/mct/Application.java
@@ -2,11 +2,10 @@ package de.sveri.mct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
-import java.applet.AppletContext;
-import java.util.Arrays;
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 /**
  * Created by sveri on 27.08.2015.
@@ -20,5 +19,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+  
     }
 }
